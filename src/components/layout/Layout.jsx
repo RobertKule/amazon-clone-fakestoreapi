633fe6dfba35b48bf7b1aeaa3e081f794efc3705
“Robert <kulewakangitsirobert@gmail.com>
2026-02-10 18:12:06 +0200

    chore: update app configuration and layout
    - Update global styles
    - Improve main application setup
    - Enhance layout component
    - Update routing structure

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -8,7 +8,7 @@ import Footer from './Footer';
  */
 const Layout = () => {
   return (
-    <div className="min-h-screen flex flex-col bg-gray-50">
+    <div className="min-h-screen flex flex-col bg-gray-50 m-0 p-0">
       {/* Header */}
       <Header />
 
